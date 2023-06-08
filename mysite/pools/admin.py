340f7cda657ee3b9c36526d6f2b5211a1329d702
avedominalilith <csgolove113377@gmail.com>
2023-06-08 23:08:56 +0300

    EXTENDED DATABASES

--- mysite/pools/admin.py
+++ mysite/pools/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Feedback
+from .models import Brand
+from .models import Protein
+from .models import Gainer
+from .models import Creatine
+from .models import Bcaa
+from .models import Bars
+
 # Register your models here.
 admin.site.register(Feedback)
+admin.site.register(Brand)
+admin.site.register(Protein)
+admin.site.register(Gainer)
+admin.site.register(Creatine)
+admin.site.register(Bcaa)
+admin.site.register(Bars)
--- mysite/pools/models.py
+++ mysite/pools/models.py
@@ -2,6 +2,47 @@ from django.db import models
 
 # Create your models here.
 class Feedback(models.Model):
-    email = models.CharField(max_length=30)
+    email = models.CharField()
     text = models.TextField()
+
+class Brand(models.Model):
+    name = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+
+class Protein(models.Model):
+    price = models.FloatField()
+    brand = models.CharField(max_length=20)
+    size = models.BigIntegerField()
+    flavour = models.CharField(max_length=20)
+    grams_of_protein = models.IntegerField()
+
+class Gainer(models.Model):
+    price = models.FloatField()
+    brand = models.CharField(max_length=20)
+    size = models.BigIntegerField()
+    flavour = models.CharField(max_length=20)
+    kcal = models.IntegerField()
+
+class Creatine(models.Model):
+    price = models.FloatField()
+    brand = models.CharField(max_length=20)
+    size = models.BigIntegerField()
+    flavour = models.CharField(max_length=20)
+
+class Bcaa(models.Model):
+    price = models.FloatField()
+    brand = models.CharField(max_length=20)
+    size = models.BigIntegerField()
+    flavour = models.CharField(max_length=20)
+
+class Bars(models.Model):
+    price = models.FloatField()
+    brand = models.CharField(max_length=20)
+    size = models.BigIntegerField()
+    flavour = models.CharField(max_length=20)
+    grams_of_protein = models.IntegerField()
+    sugarfree = models.BooleanField()
+
+
+
 
